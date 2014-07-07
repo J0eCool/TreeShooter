3385c16882bdd2f6e59c5a8ed7a608b754bc30ce
J0eCool <count.j0ecool@gmail.com>
2014-07-06 22:15:40 -0400

    Entity updating, full logic restored

--- main.hs
+++ main.hs
@@ -11,7 +11,7 @@ data Entity = Entity
 	, size :: (GLfloat, GLfloat)
 	, vel :: (GLfloat, GLfloat)
 	, onDraw :: Entity -> IO ()
-	, onUpdate :: Entity -> (Bool, Bool) -> Entity
+	, onUpdate :: Entity -> WorldState -> Entity
 	}
 
 main = do
@@ -19,8 +19,8 @@ main = do
 	_window <- createWindow "Oi"
 	worldState <- newIORef $ WorldState [
 		Entity (0.0, (-paddleDist)) (paddleWidth, paddleHeight) (0.0, 0.0) (drawEntityColor 0 1 1) updatePlayer,
-		Entity (0.0, paddleDist) (paddleWidth, paddleHeight) (0.0, 0.0) drawEntity updateNull,
-		Entity (0.0, 0.0) (ballDim, ballDim) (0.0, 1.0) drawEntity updateNull
+		Entity (0.0, paddleDist) (paddleWidth, paddleHeight) (0.0, 0.0) drawEntity updateOpponent,
+		Entity (0.0, 0.0) (ballDim, ballDim) (0.0, 1.0) drawEntity updateBall
 		] (False, False)
 	displayCallback $= display worldState
 	idleCallback $= Just (update worldState)
@@ -89,8 +89,9 @@ updateNull entity worldState = entity
 --getInputState entity =
 
 
-updatePlayer entity input = newEntity
+updatePlayer entity worldState = newEntity
 	where
+		input = keyState worldState
 		(x, y) = pos entity
 		newX =
 			case input of
@@ -99,12 +100,42 @@ updatePlayer entity input = newEntity
 				_ -> x
 		newEntity = entity { pos = (newX, y) }
 
+updateOpponent entity worldState = newEntity
+	where
+		ball = (entities worldState) !! 2
+		(bx, _) = pos ball
+		(x, y) = pos entity
+		newX = if x > bx then x - opponentSpeed else x + opponentSpeed
+		newEntity = entity { pos = (newX, y) }
+
+updateBall (Entity (bx, by) s (vx, vy) bd bu) worldState = newEntity
+	--worldState $~! \[x, y, (Entity (bx, by) s (vx, vy) bd)] ->
+	where
+		ents = entities worldState
+		t = vy > 0.0
+		padPos = getXPos $ if t then (ents !! 1) else (ents !! 0)
+		d = (bx - padPos) / paddleWidth
+		didCollidePaddle = (if t then by > paddleDist else by < (-paddleDist)) && abs d < 1.0
+		didLeaveScreen = abs by > 1.0
+		didCollideWall = bx > ballRightBound || bx < ballLeftBound
+		newV = case (didCollideWall, didCollidePaddle, didLeaveScreen) of
+			(True, _, _) -> -vx
+			(_, True, _) -> vx + d / 2
+			(_, _, True) -> vx / 2
+			_ -> vx
+		newT = if didCollidePaddle || didLeaveScreen then (-1) * vy else vy
+		dX = newV * ballSpeed
+		dY = if newT > 0 then ballSpeed else (-ballSpeed)
+		newPos = if didLeaveScreen
+			then (0.0, if t then 0.7 * paddleDist else (- 0.7) * paddleDist)
+			else (bx + dX, by + dY)
+		newEntity = Entity newPos s (newV, newT) bd bu
 
 --update :: WorldState -> IORef (Bool, Bool) -> IdleCallback
 update worldState = do
 	world <- readIORef worldState
 
-	writeIORef worldState $ WorldState (map (\e -> onUpdate e e (keyState world)) (entities world)) (keyState world)
+	writeIORef worldState $ WorldState (map (\e -> onUpdate e e world) (entities world)) (keyState world)
 
 	---- update player
 	--case ks of
@@ -113,40 +144,15 @@ update worldState = do
 	--	_ -> worldState $~! \x -> x
 
 	---- update enemy
-	--worldState $~! \[pl, Entity (x, y) s v d, (Entity (bx, by) bv t bd)] -> [pl, Entity (if x > bx then x - opponentSpeed else x + opponentSpeed, y) s v d, Entity (bx, by) bv t bd]
 
-	---- update ball
-	--worldState $~! \[x, y, (Entity (bx, by) s (vx, vy) bd)] ->
-	--	let
-	--		t = vy > 0.0
-	--		padPos = if t then getXPos y else getXPos x
-	--		d = (bx - padPos) / paddleWidth
-	--		didCollidePaddle = (if t then by > paddleDist else by < (-paddleDist)) && abs d < 1.0
-	--		didLeaveScreen = abs by > 1.0
-	--		didCollideWall = bx > ballRightBound || bx < ballLeftBound
-	--		newV = case (didCollideWall, didCollidePaddle, didLeaveScreen) of
-	--			(True, _, _) -> -vx
-	--			(_, True, _) -> vx + d / 2
-	--			(_, _, True) -> vx / 2
-	--			_ -> vx
-	--		newT = if didCollidePaddle || didLeaveScreen then (-1) * vy else vy
-	--		dX = newV * ballSpeed
-	--		dY = if newT > 0 then ballSpeed else (-ballSpeed)
-	--		newPos = if didLeaveScreen
-	--			then (0.0, if t then 0.7 * paddleDist else (- 0.7) * paddleDist)
-	--			else (bx + dX, by + dY)
-	--	in [x, y, (Entity newPos s (newV, newT) bd)]
 
 	postRedisplay Nothing
 	--where
 
 --input :: IORef (Bool, Bool) -> KeyboardMouseCallback
-input worldState key Down _ _ = case key of
-	(SpecialKey KeyLeft) -> worldState $~! \x -> x { keyState = (True, snd $ keyState x) }
-	(SpecialKey KeyRight) -> worldState $~! \x -> x { keyState = (fst $ keyState x, True) }
+input worldState key state _ _ = case key of
+	(SpecialKey KeyLeft) -> worldState $~! \x -> x { keyState = (set, snd $ keyState x) }
+	(SpecialKey KeyRight) -> worldState $~! \x -> x { keyState = (fst $ keyState x, set) }
 	_ -> return ()
-input worldState key Up _ _ = case key of
-	(SpecialKey KeyLeft) -> worldState $~! \x -> x { keyState = (False, snd $ keyState x) }
-	(SpecialKey KeyRight) -> worldState $~! \x -> x { keyState = (fst $ keyState x, False) }
-	_ -> return ()
+	where set = state == Down
 --input _ _ _ _ _ = return ()
