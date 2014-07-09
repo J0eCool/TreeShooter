2c5b72025be5f27cf8bfec86ccc0336edf686727
J0eCool <count.j0ecool@gmail.com>
2014-07-08 23:32:08 -0400

    Remove Pong, handle mouse input, add vector funcs

--- main.hs
+++ main.hs
@@ -3,10 +3,17 @@ import Data.IORef
 
 data WorldState = WorldState 
 	{ entities :: [Entity]
-	, keyState :: (Bool, Bool)
+	, heldKeys :: [Key]
+	, mousePos :: Vec2
 	}
 
 data Vec2 = Vec2 { x :: GLfloat, y :: GLfloat }
+
+(Vec2 x1 y1) /+ (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
+(Vec2 x1 y1) /- (Vec2 x2 y2) = Vec2 (x1 - x2) (y1 - y2)
+s /.* (Vec2 x y) = Vec2 (s * x) (s * y)
+(Vec2 x1 y1) /* (Vec2 x2 y2) = Vec2 (x1 * x2) (y1 * y2)
+
 
 data Entity = Entity
 	{ pos :: Vec2
@@ -16,25 +23,21 @@ data Entity = Entity
 	, onUpdate :: Entity -> WorldState -> Entity
 	}
 
+main :: IO ()
 main = do
 	(_progName, _args) <- getArgsAndInitialize
-	_window <- createWindow "Oi"
+	_window <- createWindow "Shooter"
 	worldState <- newIORef $ WorldState [
-		Entity (Vec2 0.0 (-paddleDist)) (Vec2 paddleWidth paddleHeight) (Vec2 0.0 0.0) (drawEntityColor 0 1 1) updatePlayer,
-		Entity (Vec2 0.0 paddleDist) (Vec2 paddleWidth paddleHeight) (Vec2 0.0 0.0) drawEntity updateOpponent,
-		Entity (Vec2 0.0 0.0) (Vec2 ballDim ballDim) (Vec2 0.0 1.0) drawEntity updateBall
-		] (False, False)
+		Entity (Vec2 0.0 0.0) (Vec2 shipDim shipDim) (Vec2 0.0 0.0) (drawEntityColor 0 1 0) updatePlayer
+		] [] (Vec2 0 0)
 	displayCallback $= display worldState
 	idleCallback $= Just (update worldState)
 	keyboardMouseCallback $= Just (input worldState)
+	motionCallback $= Just (mouseMotion worldState)
+	passiveMotionCallback $= Just (mouseMotion worldState)
 	mainLoop
 
-stageWidth = 0.8
-stageDim = 0.1
-paddleWidth = 0.3
-paddleHeight = 0.05
-paddleDist = 0.8
-ballDim = 0.05
+shipDim = 0.08
 
 drawRect :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> IO ()
 drawRect x y w h = do
@@ -58,85 +61,36 @@ drawEntityColor r g b e = do
 	drawEntity e
 	color $ Color3 1 1 (1 :: GLfloat)
 
---display :: IORef [Entity] -> DisplayCallback
+display :: IORef WorldState -> DisplayCallback
 display worldState = do
-	WorldState world _ <- readIORef worldState
+	WorldState entities _ _ <- readIORef worldState
 	clear [ColorBuffer]
 	loadIdentity
 
-	mapM_ (\e -> (onDraw e) e) world
-
-	-- draw walls
-	drawRect (-stageWidth) 0 stageDim 2.0
-	drawRect stageWidth 0 stageDim 2.0
+	mapM_ (\e -> (onDraw e) e) entities
 
 	flush
 
+clamp :: (Ord a) => a -> a -> a -> a
 clamp t lo hi
 	| t < lo = lo
 	| t > hi = hi
 	| otherwise = t
 
-getXPos = x . pos
-
-playerSpeed = 0.0002
-opponentSpeed = 0.0001
-ballSpeed = 0.0003
-left = (stageDim / 2) - stageWidth
-right = stageWidth - (stageDim / 2)
-paddleLeftBound = (paddleWidth / 2) + left
-paddleRightBound = right - (paddleWidth / 2)
-ballLeftBound = (ballDim / 2) + left
-ballRightBound = right - (ballDim / 2)
-
+updateNull :: Entity -> WorldState -> Entity
 updateNull entity worldState = entity
 
---getInputState entity =
-
-
-updatePlayer entity worldState = newEntity
+updatePlayer :: Entity -> WorldState -> Entity
+updatePlayer entity worldState = entity { pos = newPos } { size = newSize }
 	where
-		input = keyState worldState
-		Vec2 x y = pos entity
-		newX =
-			case input of
-				(True, False) -> clamp (x - playerSpeed) paddleLeftBound paddleRightBound
-				(False, True) -> clamp (x + playerSpeed) paddleLeftBound paddleRightBound
-				_ -> x
-		newEntity = entity { pos = Vec2 newX y }
+		m = mousePos worldState
+		newPos = Vec2 2 (-2) /* (m /- Vec2 0.5 0.5)
 
-updateOpponent entity worldState = newEntity
-	where
-		ball = (entities worldState) !! 2
-		bx = x $ pos ball
-		Vec2 ex ey = pos entity
-		newX = if ex > bx then ex - opponentSpeed else ex + opponentSpeed
-		newEntity = entity { pos = Vec2 newX ey }
-
-updateBall entity worldState = newEntity
-	where
-		Vec2 bx by = pos entity
-		Vec2 vx vy = vel entity
-		ents = entities worldState
-		t = vy > 0.0
-		padPos = getXPos $ if t then (ents !! 1) else (ents !! 0)
-		d = (bx - padPos) / paddleWidth
-		didCollidePaddle = (if t then by > paddleDist else by < (-paddleDist)) && abs d < 1.0
-		didLeaveScreen = abs by > 1.0
-		didCollideWall = bx > ballRightBound || bx < ballLeftBound
-		newVX = case (didCollideWall, didCollidePaddle, didLeaveScreen) of
-			(True, _, _) -> -vx
-			(_, True, _) -> vx + d / 2
-			(_, _, True) -> vx / 2
-			_ -> vx
-		newVY = if didCollidePaddle || didLeaveScreen then (-1) * vy else vy
-		newVel = Vec2 newVX newVY
-		dX = newVX * ballSpeed
-		dY = if newVY > 0 then ballSpeed else (-ballSpeed)
-		newPos = if didLeaveScreen
-			then Vec2 0.0 $ if t then 0.7 * paddleDist else (- 0.7) * paddleDist
-			else Vec2 (bx + dX) (by + dY)
-		newEntity = entity { pos = newPos } { vel = newVel }
+		keys = heldKeys worldState
+		isHeld x = not $ filter (== x) keys == []
+		newSize = Vec2
+			(if isHeld (Char 'a') then shipDim * 0.5 else shipDim)
+			(if isHeld (MouseButton LeftButton) then 2 * shipDim else (if isHeld (Char 'g') then shipDim * 0.5 else shipDim))
 
 update :: IORef WorldState -> IdleCallback
 update worldState = do
@@ -146,10 +100,18 @@ update worldState = do
 
 	postRedisplay Nothing
 
---input :: IORef (Bool, Bool) -> KeyboardMouseCallback
-input worldState key state _ _ = case key of
-	(SpecialKey KeyLeft) -> worldState $~! \x -> x { keyState = (set, snd $ keyState x) }
-	(SpecialKey KeyRight) -> worldState $~! \x -> x { keyState = (fst $ keyState x, set) }
-	_ -> return ()
-	where set = state == Down
---input _ _ _ _ _ = return ()
+input :: IORef WorldState -> KeyboardMouseCallback
+input worldState key state _ _ = do
+	world <- readIORef worldState
+	let
+		f = if state == Down
+			then \l -> key : l
+			else \l -> filter (not . (== key)) l
+		nowHeld = f $ heldKeys world
+	worldState $~! \w -> w { heldKeys = nowHeld }
+
+mouseMotion :: IORef WorldState -> MotionCallback
+mouseMotion worldState (Position x y) = do
+	Size wx wy <- get windowSize
+	let winPos = Vec2 ((fromIntegral x) / (fromIntegral wx)) ((fromIntegral y) / (fromIntegral wy))
+	worldState $~! \w -> w { mousePos = winPos }
