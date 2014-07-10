69c6a3f7874d1b281f1dbd4f8dbebbb573516681
J0eCool <count.j0ecool@gmail.com>
2014-07-10 01:30:51 -0400

    Handle add/remove entities on update

--- main.hs
+++ main.hs
@@ -4,24 +4,35 @@ import Data.IORef
 data WorldState = WorldState 
 	{ entities :: [Entity]
 	, heldKeys :: [Key]
+	, pressedKeys :: [Key]
 	, mousePos :: Vec2
 	}
 
-data Vec2 = Vec2 { x :: GLfloat, y :: GLfloat }
+data Vec2 = Vec2
+	{ x :: GLfloat
+	, y :: GLfloat
+	} deriving (Eq, Show)
 
 (Vec2 x1 y1) /+ (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
 (Vec2 x1 y1) /- (Vec2 x2 y2) = Vec2 (x1 - x2) (y1 - y2)
 s /.* (Vec2 x y) = Vec2 (s * x) (s * y)
 (Vec2 x1 y1) /* (Vec2 x2 y2) = Vec2 (x1 * x2) (y1 * y2)
 
+data UpdateData = UpdateData
+	{ newState :: Entity
+	, toAdd :: [Entity]
+	, toRemove :: [Entity]
+	}
 
 data Entity = Entity
 	{ pos :: Vec2
 	, size :: Vec2
 	, vel :: Vec2
 	, onDraw :: Entity -> IO ()
-	, onUpdate :: Entity -> WorldState -> Entity
+	, onUpdate :: Entity -> WorldState -> UpdateData
 	}
+instance Eq Entity where
+	Entity p1 s1 v1 _ _ == Entity p2 s2 v2 _ _ = (p1 == p2) && (s1 == s2) && (v1 == v2)
 
 main :: IO ()
 main = do
@@ -29,7 +40,7 @@ main = do
 	_window <- createWindow "Shooter"
 	worldState <- newIORef $ WorldState [
 		Entity (Vec2 0.0 0.0) (Vec2 shipDim shipDim) (Vec2 0.0 0.0) (drawEntityColor 0 1 0) updatePlayer
-		] [] (Vec2 0 0)
+		] [] [] (Vec2 0 0)
 	displayCallback $= display worldState
 	idleCallback $= Just (update worldState)
 	keyboardMouseCallback $= Just (input worldState)
@@ -63,11 +74,11 @@ drawEntityColor r g b e = do
 
 display :: IORef WorldState -> DisplayCallback
 display worldState = do
-	WorldState entities _ _ <- readIORef worldState
+	world <- readIORef worldState
 	clear [ColorBuffer]
 	loadIdentity
 
-	mapM_ (\e -> (onDraw e) e) entities
+	mapM_ (\e -> (onDraw e) e) (entities world)
 
 	flush
 
@@ -77,26 +88,47 @@ clamp t lo hi
 	| t > hi = hi
 	| otherwise = t
 
-updateNull :: Entity -> WorldState -> Entity
-updateNull entity worldState = entity
+updateNull :: Entity -> WorldState -> UpdateData
+updateNull entity worldState = UpdateData entity [] []
 
-updatePlayer :: Entity -> WorldState -> Entity
-updatePlayer entity worldState = entity { pos = newPos } { size = newSize }
+updateVel :: Entity -> WorldState -> UpdateData
+updateVel entity worldState = UpdateData newEntity [] []
+	where newEntity = entity { pos = pos entity /+ vel entity }
+
+updatePlayer :: Entity -> WorldState -> UpdateData
+updatePlayer entity worldState = UpdateData newEntity added removed
 	where
 		m = mousePos worldState
 		newPos = Vec2 2 (-2) /* (m /- Vec2 0.5 0.5)
 
-		keys = heldKeys worldState
-		isHeld x = not $ filter (== x) keys == []
+		isHeld x = x `elem` heldKeys worldState
+		isDown x = x `elem` pressedKeys worldState
 		newSize = Vec2
 			(if isHeld (Char 'a') then shipDim * 0.5 else shipDim)
 			(if isHeld (MouseButton LeftButton) then 2 * shipDim else (if isHeld (Char 'g') then shipDim * 0.5 else shipDim))
+
+		newEntity = entity { pos = newPos } { size = newSize }
+
+		added = if isDown (MouseButton LeftButton)
+			then [Entity newPos (Vec2 0.025 0.025) (Vec2 0 0.001) (drawEntityColor 1 0 0) updateVel]
+			else []
+
+		ents = entities worldState
+		removed = if length ents > 5
+			then [ents !! 1]
+			else []
 
 update :: IORef WorldState -> IdleCallback
 update worldState = do
 	world <- readIORef worldState
 
-	writeIORef worldState $ world { entities = map (\e -> onUpdate e e world) (entities world) }
+	windowTitle $= "Shooter " ++ (show $ length $ entities world)
+
+	writeIORef worldState $ let
+		compact updates = foldr (\(UpdateData e a r) (es, as, rs) -> (e:es, a ++ as, r ++ rs)) ([], [], []) updates
+		(ents, adds, rems) = compact $ map (\e -> onUpdate e e world) (entities world)
+		newEnts = filter (not . (`elem` rems)) (ents ++ adds)
+		in world { entities = newEnts } { pressedKeys = []}
 
 	postRedisplay Nothing
 
@@ -108,7 +140,11 @@ input worldState key state _ _ = do
 			then \l -> key : l
 			else \l -> filter (not . (== key)) l
 		nowHeld = f $ heldKeys world
-	worldState $~! \w -> w { heldKeys = nowHeld }
+		f' = if state == Down
+			then \l -> key : l
+			else id
+		nowPressed = f' $ pressedKeys world
+	worldState $~! \w -> w { heldKeys = nowHeld } { pressedKeys = nowPressed }
 
 mouseMotion :: IORef WorldState -> MotionCallback
 mouseMotion worldState (Position x y) = do
